b3f9202c1c2299875ef1f5360cb15ea8853d71ea
skonstantin1234 <134043005+skonstantin1234@users.noreply.github.com>
2023-05-19 10:21:42 +0300

    Update streamlit_app.py
    Added new breakfast menu and 3 items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
 
-streamlit.title('My Streamlit and Snowflake Environments')
-streamlit.header('My Menu')
-streamlit.text('One way')
+streamlit.title('My Perants New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
